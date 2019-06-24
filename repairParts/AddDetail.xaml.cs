6f3fe523a08e594888c66734759d06c9379ea9bb
Boris <51057023+bo90@users.noreply.github.com>
2019-06-24 19:45:42 +0300

    Добавьте файлы проекта.

---
+++ repairParts/AddDetail.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.Odbc;
+
+namespace repairParts
+{
+    /// <summary>
+    /// Логика взаимодействия для AddDetail.xaml
+    /// </summary>
+    public partial class AddDetail : Window
+    {
+        /*строка подключения*/
+        public static string connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=repairDB.mdb;";
+        private OleDbConnection myConnection;
+
+        public AddDetail()
+        {
+            InitializeComponent();
+            myConnection = new OleDbConnection(connString);
+            myConnection.Open();
+        }
+
+        private void ComboBox_DropDownClosed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadingNameDetail()
+        {
+            try
+            {
+                string query = "SELECT namePair FROM Pairs";
+                OleDbCommand comm = new OleDbCommand(query, myConnection);
+                OleDbDataAdapter da = new OleDbDataAdapter(comm);
+                DataSet ds = new DataSet();
+                da.Fill(ds, query);
+                nameDateil.ItemsSource = ds.Tables[0].DefaultView;
+
+                query = "SELECT ID_Pair FROM Pairs";
+                comm = new OleDbCommand(query, myConnection);
+                da = new OleDbDataAdapter(comm);
+                ds = new DataSet();
+                da.Fill(ds, query);
+                partNumber.ItemsSource = ds.Tables[0].DefaultView;
+
+                //firmaComBox
+                query = "SELECT NameFirm FROM Pairs";
+                comm = new OleDbCommand(query, myConnection);
+                da = new OleDbDataAdapter(comm);
+                ds = new DataSet();
+                da.Fill(ds, query);
+                firmaComBox.ItemsSource = ds.Tables[0].DefaultView;
+
+                //IndexFirm
+                query = "SELECT IndexFirm FROM Firms";
+                comm = new OleDbCommand(query, myConnection);
+                da = new OleDbDataAdapter(comm);
+                ds = new DataSet();
+                da.Fill(ds, query);
+                firmIDComBox.ItemsSource = ds.Tables[0].DefaultView;
+
+                //NameFirm
+                query = "SELECT NameFirm FROM Firms";
+                comm = new OleDbCommand(query, myConnection);
+                da = new OleDbDataAdapter(comm);
+                ds = new DataSet();
+                da.Fill(ds, query);
+                fullNmaeComBox.ItemsSource = ds.Tables[0].DefaultView;
+
+                //Mark
+                query = "SELECT Mark FROM CarTable";
+                comm = new OleDbCommand(query, myConnection);
+                da = new OleDbDataAdapter(comm);
+                ds = new DataSet();
+                da.Fill(ds, query);
+                markcomBox.ItemsSource = ds.Tables[0].DefaultView;
+
+                //PetName
+                query = "SELECT PetName FROM CarTable";
+                comm = new OleDbCommand(query, myConnection);
+                da = new OleDbDataAdapter(comm);
+                ds = new DataSet();
+                da.Fill(ds, query);
+                ModelComBox.ItemsSource = ds.Tables[0].DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadingNameDetail();
+        }
+
+        
+    }
+}
---
+++ repairParts/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Data;
+using System.Threading;
+
+namespace repairParts
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            myConnection = new OleDbConnection(connString);
+            myConnection.Open();
+        }
+
+        public static string connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=repairDB.mdb;";
+        private OleDbConnection myConnection;
+
+        private async void LoadTable()
+        {
+            await this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, (ThreadStart)delegate ()
+           {
+               string query = "SELECT * FROM Pairs";
+               OleDbCommand command = new OleDbCommand(query, myConnection);
+               OleDbDataAdapter da = new OleDbDataAdapter(command);
+               DataSet ds = new DataSet();
+               da.Fill(ds, query);
+               dg.ItemsSource = ds.Tables[0].DefaultView;
+               
+           });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddDetail aDet = new AddDetail();
+            aDet.Show();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(() => LoadTable());
+        }
+
+        private string queryToSeek;
+
+        private void seek(int record)
+        {
+            try
+            {
+                if (record == 0) /*поиск по парт № детали*/
+                {
+                    string param = searchTxtBox.Text;
+                    queryToSeek = "SELECT * FROM Pairs Where IndexPair ='" + param + "' ";
+                }
+                if (record == 1) /*поиск по номеру детали*/
+                {
+                    int param = Convert.ToInt32(searchTxtBox.Text);
+                    string query = "SELECT * FROM Pairs Where id ='" + param + "' ";
+                }
+                if (record == 2) /*поиск по фирме*/
+                {
+                    string param = searchTxtBox.Text;
+                    queryToSeek = "SELECT * FROM Pairs Where NameFirm ='" + param + "'";
+                }
+                seekRecor();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+        }
+
+        private void seekRecor()
+        {
+            try
+            {
+                dg.ItemsSource = null;                
+                OleDbCommand command = new OleDbCommand(queryToSeek, myConnection);
+                OleDbDataAdapter da = new OleDbDataAdapter(command);
+                DataSet ds = new DataSet();
+                da.Fill(ds, queryToSeek);
+                dg.ItemsSource = ds.Tables[0].DefaultView;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;           
+            }
+        }
+
+       
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            myConnection.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            seek(searchCmbBox.SelectedIndex);
+        }
+    }
+}
